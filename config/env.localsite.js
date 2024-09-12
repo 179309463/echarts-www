654cd5dd84bdde02775a0b346ab886ffffb4bebb
fengzhuojia <179309463@qq.com>
2024-09-12 14:54:33 +0800

    调整localsite

--- config/env.localsite.js
+++ config/env.localsite.js
@@ -1,19 +1,18 @@
 const path = require('path');
 const config = require('./common');
 
-config.host = 'http://localhost/echarts-website';
-// config.host = 'http://localhost:8000/echarts/echarts-website';
+config.host = '';
 
 config.cdnPayRootMap = {
     zh: config.host,
-    en: 'http://localhost/echarts-website'
+    en: ''
 };
 config.cdnFreeRootMap = {
     zh: config.host,
     en: config.host
 };
 
-config.galleryPath = 'https://echarts.apache.org/examples/';
+config.galleryPath = '/examples/';
 config.releaseDestDir = path.resolve(__dirname, '../../echarts-website');
 
 module.exports = config;
